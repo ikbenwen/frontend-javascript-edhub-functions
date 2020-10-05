508133918e378f3b6e5d2e69ad686f9051f894e3
ikbenwen <w.michels@novi-education.nl>
2020-10-05 09:30:35 +0200

    opdrachten verbeterd.

--- functions.js
+++ functions.js
@@ -4,24 +4,19 @@
 // 3, 4, 5 geeft 12
 // 11, 3, 9 geeft 23
 function optellen (getal1, getal2, getal3){
-    const optelsom = getal1 + getal2 + getal3
-    return optelsom
+    return getal1 + getal2 + getal3
 }
-const rekenUit = optellen(3,4,5)
-console.log(rekenUit);
+console.log(optellen(1,2,3));
 
 /* Opdracht 2 */
 // Schrijf een functie die twee strings verwacht en deze aan elkaar geplakt teruggeeft.
 // Je mag hier geen String Object methoden voor gebruiken.
 // ---- Verwachte uitkomsten:
 // "abra", "cadabra" geeft "abracadabra"
-function stringPlakken (woord1, woord2) {
-    const woorden = woord1 + woord2
-    return woorden
+function toverspreuk (woord1, woord2) {
+    return woord1 + woord2
 }
-const toverspreuk = stringPlakken("abra", "cadabra");
-console.log(toverspreuk);
-
+console.log(toverspreuk("abra", "cadabra"));
 
 /* Opdracht 3 */
 // Schrijf een functie die twee parameters verwacht: een woord en een karakter
@@ -31,11 +26,10 @@ console.log(toverspreuk);
 // "beep", "_" geeft "_beep_"
 // "kaas", "Q" geeft "QkaasQ"
 function omwikkelen (woord, karakter) {
-    const voorAchter = karakter+woord+karakter
-    return voorAchter
+    return karakter+woord+karakter
 }
-const geplakt = omwikkelen("hoi","!");
-console.log(geplakt);
+
+console.log(omwikkelen("hoi","!"));
 
 /* Opdracht 4 */
 // Schrijf een functie die een array van strings verwacht. De functie geeft
@@ -45,13 +39,21 @@ console.log(geplakt);
 // ["abra", "cadabra"] geeft "abracadabra"
 // ["a", "b", "c"] geeft "abc"
 function alphabet  (letter1, letter2, letter3) {
-    const letters = letter1+letter2+letter3
-    return letters
+    return letter1+letter2+letter3
 }
 
-const alleLetters = alphabet("a","b","c");
-console.log(alleLetters);
+console.log(alphabet("a","b","c"));
+//boven mijn oplossing onder oplossing van het antwoordenblad.
 
+function concatenate(wordArray) {
+    let output = "";
+
+    for (let i = 0; i < wordArray.length; i++) {
+        output = output + wordArray[i];
+    }
+    return output;
+}
+console.log(concatenate(["a", "b", "c"]));
 /* Opdracht 5 */
 // Schrijf een functie die een array van strings verwacht en een gezamelijke groet teruggeeft
 // ---- Verwachte uitkomsten:
@@ -67,3 +69,28 @@ function begroetting (groet, naam1, naam2, naam3) {
 const halloDaar = begroetting("hoi","piet","kees","henk");
 console.log(halloDaar);
 
+// boven mijn uitwerking, onder de juiste.
+
+function greeting(stringsArray) {
+    let greeting = "Hoi ";
+
+    for (let i = 0; i < stringsArray.length; i++) {
+        // als dit de laatste entry is, voeg dan het uitroepteken toe
+        if (i === stringsArray.length - 1) {
+            return greeting = greeting + "en " + stringsArray[i] + "!";
+        }
+        // als de array maar twee namen bevat, voeg dan geen komma toe
+        if (stringsArray.length === 2) {
+            greeting = greeting + stringsArray[i] + " ";
+        }
+        // als de array meer dan twee namen bevat, voeg dan wel een komma toe
+        if (stringsArray.length > 2) {
+            greeting = greeting + stringsArray[i] + ", ";
+        }
+    }
+
+    return greeting;
+}
+
+
+console.log(greeting(["Piet", "Henk"]));
